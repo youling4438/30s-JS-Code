6b475955edeffd4401b4df406cd36804faa1436f
youling4438 <youling0106@qq.com>
2019-01-20 22:17:20 +0800

    remove yeoman img at app component

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,14 +3,14 @@ require('styles/App.css');
 
 import React from 'react';
 import Header from './Header';
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
 	render() {
 		return (
 			<div className="index">
 				<Header />
-				<img src={yeomanImage} alt="Yeoman Generator" />
+				{/* <img src={yeomanImage} alt="Yeoman Generator" /> */}
 				<div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
 			</div>
 		);
