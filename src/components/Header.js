2babdc48bce170d5f499454f21822a17b93b54a4
youling4438 <youling0106@qq.com>
2019-01-20 22:18:50 +0800

    import img for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 require('styles/Header.css');
 
 import React from 'react';
+import codeImage from '../images/logo_256.png'
+import githubImage from '../images/github.png'
 
 class HeaderComponent extends React.Component {
     render() {
         return (
             <div className="title">
                 <div className="left">...</div>
-                <div className="center">30 seconds of code</div>
+                <div className="center">
+                    <img src={codeImage} alt="logo-image" />
+                    <span>30 seconds of code</span>
+                </div>
                 <div className="right">
                     <a>
-                        IMG
+                        <img src={githubImage} alt="github-image" />
                     </a>
                 </div>
             </div>
