7c46d55adb4e0f1ab7f15fcbfb8b8f4c22ed9730
youling <youling0106@qq.com>
2019-01-21 23:32:30 +0800

    add CodeCard Component

---
+++ src/components/CodeCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class CodeCardComponent extends React.Component {
+    constructor(props){
+        super(props);
+
+    }
+    render() {
+        const { fnData } = this.props;
+        const { fnName, fnDescription, fnCode, fnExampleCode } = fnData;
+        const fnExampleCodeList = fnExampleCode.map((item)=><p>{item}</p>);
+        return (
+            <div className="codeCard">
+                <div className="fnName">{fnName}</div>
+                <div className="fnDescription">{fnDescription}</div>
+                <div className="fnCode">{fnCode}</div>
+                <div className="fnCode">{fnExampleCodeList}</div>
+            </div>
+        );
+    }
+}
+
+CodeCardComponent.defaultProps = {
+};
+
+export default CodeCardComponent;
