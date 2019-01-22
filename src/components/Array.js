39382564f2e0f8bd05165cd27fdd20cd9fb24ab6
youling4438 <youling0106@qq.com>
2019-01-22 23:26:19 +0800

    import source data for ArrayComponent

--- src/components/Array.js
+++ src/components/Array.js
@@ -1,31 +1,10 @@
 import React from 'react';
 import CodeCard from './CodeCard';
+import ArrayData from '../sources/Array'
 
 class ArrayComponent extends React.Component {
     render() {
-        const fnlist = [
-            {
-                fnName: 'all',
-                fnDescription: `Returns true if the provided predicate function returns true for all elements in a collection, false otherwise.
-                    Use Array.prototype.every() to test if all elements in the collection return true based on fn.Omit the second argument, fn,
-                    to use Boolean as a default.`,
-                fnCode: 'const all = (arr, fn = Boolean) => arr.every(fn);',
-                fnExampleCode: [
-                    'all([4, 2, 3], x => x > 1); // true',
-                    'all([1, 2, 3]); // true'
-                ]
-            },
-            {
-                fnName: 'allEqual',
-                fnDescription: `Check if all elements in an array are equal.
-                    Use Array.prototype.every() to check if all the elements of the array are the same as the first one.`,
-                fnCode: 'const allEqual = arr => arr.every(val => val === arr[0]);',
-                fnExampleCode: [
-                    'allEqual([1, 2, 3, 4, 5, 6]); // false',
-                    'allEqual([1, 1, 1, 1]); // true'
-                ]
-            }
-        ];
+        const fnlist = ArrayData.fnlist;
         const fnListDom = fnlist.map(item => <CodeCard fnData={item} />);
         return (
             <div className="container">
