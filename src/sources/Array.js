7c275d5de2ef2f3827f30dadec7bfe03d2dc6347
youling4438 <youling0106@qq.com>
2019-01-22 23:43:43 +0800

    add fn list

--- src/sources/Array.js
+++ src/sources/Array.js
@@ -31,6 +31,39 @@ export default {
                 'any([0, 1, 2, 0], x => x >= 2); // true',
                 'any([0, 0, 1, 0]); // true'
             ]
+        },
+        {
+            fnName: 'chunk',
+            fnDescription: `Chunks an array into smaller arrays of a specified size.
+                Use Array.from() to create a new array, that fits the number of chunks that will be produced.
+                Use Array.prototype.slice() to map each element of the new array to a chunk the length of size.
+                If the original array can't be split evenly, the final chunk will contain the remaining elements.`,
+            fnCode: `const chunk = (arr, size) =>
+                        Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+                        arr.slice(i * size, i * size + size)
+                    );`,
+            fnExampleCode: [
+                'chunk([1, 2, 3, 4, 5], 2); // [[1,2],[3,4],[5]]'
+            ]
+        },
+        {
+            fnName: 'compact',
+            fnDescription: `Removes falsey values from an array.
+                Use Array.prototype.filter() to filter out falsey values (false, null, 0, "", undefined, and NaN).`,
+            fnCode: 'const compact = arr => arr.filter(Boolean);',
+            fnExampleCode: [
+                'compact([0, 1, false, 2, "", 3, "a", "e" * 23, NaN, "s", 34]); // [ 1, 2, 3, "a", "s", 34 ]'
+            ]
+        },
+        {
+            fnName: 'deepFlatten',
+            fnDescription: `Deep flattens an array. 
+                Use recursion. Use Array.prototype.concat() with an empty array ([]) and the spread operator (...) to flatten an array. 
+                Recursively flatten each element that is an array.`,
+            fnCode: 'const deepFlatten = arr => [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));',
+            fnExampleCode: [
+                'deepFlatten([1, [2], [[3], 4], 5]); // [1,2,3,4,5]'
+            ]
         }
     ]
 }
