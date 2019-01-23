f4913a4a2b2cc8d10118cce25544a13b5378ce43
youling4438 <youling0106@qq.com>
2019-01-23 23:51:37 +0800

    add function to switch component

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,12 +7,35 @@ import Array from './Array';
 import Date from './Date';
 
 class AppComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showComponentName: 'Array'
+		}
+		this.buttonClickHandle = this.buttonClickHandle.bind(this);
+	}
+
+	buttonClickHandle(showComponentName) {
+		this.setState({
+			showComponentName
+		});
+	}
+
 	render() {
+		const { showComponentName } = this.state;
+		const allComponents = {
+			'Array': <Array />,
+			'Date': <Date />
+		};
+		const showComponent = allComponents[showComponentName];
 		return (
 			<div className="index">
 				<Header />
-				<Array />
-				<Date />
+				{showComponent}
+				<div>
+					<button onClick={() => this.buttonClickHandle('Array')}>Array</button>
+					<button onClick={() => this.buttonClickHandle('Date')}>Date</button>
+				</div>
 				<div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
 			</div>
 		);
