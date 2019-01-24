a5dd31afab02015f67ad7e3ef0cbf8d54e2f7d28
youling4438 <youling0106@qq.com>
2019-01-24 23:24:38 +0800

    add function for buttongroup

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -3,15 +3,12 @@ require('styles/ButtonGroup.css');
 import React from 'react';
 
 class ButtonGroupComponent extends React.Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         const { buttonClickHandle } = this.props;
         return (
             <div className="buttonGrounp">
                 <a className="button" onClick={() => buttonClickHandle('Array')}>Array</a>
-                <a className="button" onClick={() => buttonClickHandle('Array')}>Date</a>
+                <a className="button" onClick={() => buttonClickHandle('Date')}>Date</a>
             </div>
         );
     }
